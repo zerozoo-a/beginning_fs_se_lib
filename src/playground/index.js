e44f6cf83f86df86576b9e4a37e42b28e41481bb
zerozoo <zerozoo385@gmail.com>
2022-09-24 13:20:24 +0900

    feat(some.fn): some 함수 구현

--- src/playground/index.js
+++ src/playground/index.js
@@ -1,4 +1,5 @@
 import { every } from "../Lazy/every.js";
+import { some } from "../some.js";
 
 const a = [1, 2, 3];
 const b = [true, false, false, true];
@@ -7,3 +8,6 @@ const predicate = (a) => a > 0;
 const res = every(b, (a) => true && a);
 const rr = [...res];
 console.log("rr", rr);
+
+const aa = some(b, (a) => a);
+console.log("aa", aa);
